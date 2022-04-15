17e45e94d3b6b84d3c71dfaa26522a62d87ebb5b
Ahmad Romy <ahmadromyzahran@gmail.com>
2022-04-15 15:31:34 +0700

    lupa declare var di verdict

--- services/dna-test-verdict.js
+++ services/dna-test-verdict.js
@@ -10,7 +10,7 @@ async function verdictDNA(dnaPenyakit, dnaUser) {
         };
         return verdict;
     } else {
-        lcs_len = lcs(dnaPenyakit, dnaUser); // longest common subsequence dalam O(n*m)
+        const lcs_len = lcs(dnaPenyakit, dnaUser); // longest common subsequence dalam O(n*m)
         const verdict = {
             similarity : 100*lcs_len/dnaPenyakit.length,
             status : (this.similarity>=80)

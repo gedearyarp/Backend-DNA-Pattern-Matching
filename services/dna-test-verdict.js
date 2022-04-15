557ca6ee82600b474b1814adc93ca8732d9bcddd
Ahmad Romy <ahmadromyzahran@gmail.com>
2022-04-15 15:29:29 +0700

    add string-algo (kmp,lcs), implement verdictDNA

--- services/dna-test-verdict.js
+++ services/dna-test-verdict.js
@@ -1,5 +1,22 @@
+const {kmp, lcs} = require('../lib/string-algo');
+
 async function verdictDNA(dnaPenyakit, dnaUser) {
     // TODO: menghitung similarity dan status dari dna penyakit dan dna pengguna
+    const idx = kmp(dnaPenyakit, dnaUser); // string matching dalam O(n+m)
+    if(idx>=0) {
+        const verdict = {
+            similarity : 100,
+            status : true
+        };
+        return verdict;
+    } else {
+        lcs_len = lcs(dnaPenyakit, dnaUser); // longest common subsequence dalam O(n*m)
+        const verdict = {
+            similarity : 100*lcs_len/dnaPenyakit.length,
+            status : (this.similarity>=80)
+        };
+        return verdict;
+    }
 }
 
 module.exports = verdictDNA;

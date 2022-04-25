ebbe3220d2f375cf791867bbf8ddd46c3443f175
Ahmad Romy <ahmadromyzahran@gmail.com>
2022-04-26 05:15:09 +0700

    fix check tanggal in search-dna

--- services/search-dna.service.js
+++ services/search-dna.service.js
@@ -1,14 +1,20 @@
 const DNATest = require('../models/dna-test');
 
+const checkFormat1 = (tanggal,val) => {
+    const options = {day: "numeric", month: "long", year: "numeric"};
+    const date = new Date(val);
+    return tanggal.toLowerCase() === date.toLocaleString("id-ID", options).toLowerCase();
+};
+
 async function seacrhDNA(inputUser) {
     // TODO: mengembalikan list id test dna yang sesuai dengan input user
     // untuk akses data dna test, gunakan method find agar dapat list data keseluruhan (cari google kalo bingung)
-    const listTesDNA = await DNATest.find()
-    const regexp = /(?<tanggal>^\d{1,2} [A-Z][a-z]* \d{4})? ?(?<namaPenyakit>[A-Za-z 0-9]+$)?/
+    const listTesDNA = await DNATest.find();
+    const regexp = /(?<tanggal>^\d{1,2} [A-Z][a-z]* \d{4})? ?(?<namaPenyakit>[A-Za-z 0-9]+$)?/i;
     const query = inputUser.match(regexp);
     return listTesDNA.filter(dna => {
-        (query.tanggal===undefined || query.tanggal===dna.tanggal) && (query.namaPenyakit===undefined || query.namaPenyakit===dna.namaPenyakit)
-    }).map(dna => dna._id); // note: kalo input user="", return semua data
+        (query.tanggal===undefined || checkFormat1(query.tanggal,dna.tanggal)) && (query.namaPenyakit===undefined || query.namaPenyakit.toLowerCase()===dna.namaPenyakit.toLowerCase());
+    }).map(dna => dna._id); // note: kalo inputUser="", return semua data
 }
 
 module.exports = seacrhDNA;

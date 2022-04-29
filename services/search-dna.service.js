ebebb8af22c7d204b07793804114b5db7a7998ca
Ahmad Romy <ahmadromyzahran@gmail.com>
2022-04-29 08:42:29 +0700

    checkformat versi tanggal string

--- services/search-dna.service.js
+++ services/search-dna.service.js
@@ -17,6 +17,31 @@ const checkFormat1 = (tanggal,val) => {
             tanggal.split('-').join('/') === date.toLocaleString("id-ID", options2);
 };
 
+const checkFormatSTR = (tanggal,strTanggal) => {
+    if(tanggal.toLowerCase() === strTanggal.toLowerCase()) {
+        return true;
+    }
+    const monthIndex = {
+        'January': 1,
+        'February': 2,
+        'March': 3,
+        'April': 4,
+        'May': 5,
+        'June': 6,
+        'July': 7,
+        'August': 8,
+        'September': 9,
+        'October': 10,
+        'November': 11,
+        'December': 12
+    };
+    let dmy = strTanggal.split(' ');
+    dmy[0] = String(parseInt(dmy[0],10)).padStart(2, '0');
+    dmy[1] = String(parseInt(monthIndex[dmy[1]],10)).padStart(2, '0');
+    dmy[2] = String(parseInt(dmy[2],10)).padStart(4, '0');
+    return tanggal===dmy.join('/') || tanggal===dmy.join('-');
+};
+
 async function searchDNA(inputUser) {
     const listTesDNA = await DNATest.find();
     const regexp = /(?<tanggal>^\d{1,2} [A-Z][a-z]* \d{4}|^\d{2}-\d{2}-\d{4}|^\d{2}\/\d{2}\/\d{4})? ?(?<namaPenyakit>[A-Za-z 0-9]+$)?/i;
@@ -41,5 +66,15 @@ async function searchDNA(inputUser) {
             )
         );
 }
+
+// const s1 = "26 april 2022";
+// const s2 = "26/04/2022";
+// const s3 = "26-04-2022";
+
+// const db = "26 April 2022";
+
+// console.log(checkFormatSTR(s1,db));
+// console.log(checkFormatSTR(s2,db));
+// console.log(checkFormatSTR(s3,db));
 
 module.exports = searchDNA;
